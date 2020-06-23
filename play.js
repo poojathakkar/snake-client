1db7fe0503268ef2b3eff1ad8c1ac3285e45e5fb
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-23 12:59:09 +0000

    refactored

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const net = require('net');
-
+const connect = require('./client');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
-
-  return conn;
-}
-
 console.log('Connecting ...');
 connect();
