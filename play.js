de84e84c61350bb6ef39e7bfe0147eabcda07db4
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-23 18:26:46 +0000

    added stdin

--- play.js
+++ play.js
@@ -1,6 +1,29 @@
-const connect = require('./client');
+const { connect } = require('./client');
 /**
  * Establishes connection with the game server
  */
 console.log('Connecting ...');
 connect();
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+  handleUserInput();
+  return stdin;
+};
+
+ 
+setupInput();
+
