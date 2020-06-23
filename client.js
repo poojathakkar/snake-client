fd2c3d8e767b527ba7bb508389df45e7561f4cdb
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-23 13:00:35 +0000

    connect refactor

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('connect', () => {
+    conn.write('Hello from client!');
+  });
+
+  return conn;
+}
+
+module.exports = connect;
