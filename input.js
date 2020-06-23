369ddc1e4ee7752ecf75fd02c3ea4d8c49c67d69
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-23 18:41:03 +0000

    added setUpInput()

---
+++ input.js
@@ -0,0 +1,21 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+  handleUserInput();
+  return stdin;
+};
+
+module.exports = { setupInput };
