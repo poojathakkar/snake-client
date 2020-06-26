b870eb6b2cddb0aa361f57ab07eecf63b19b8904
Pooja Thakkar <poojapthakkar@gmail.com>
2020-06-26 00:26:49 +0000

    added setUpInput

--- input.js
+++ input.js
@@ -1,21 +1,25 @@
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
+let connection;
+const net = require('net');
+
+const handleUserInput = function(key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+  if(key === 'w') {connection.write('Move: up')};
+  if(key === 'a') {connection.write('Move: up')};
+  if(key === 's') {connection.write('Move: left')};
+  if(key === 'd') {connection.write('Move: right')};
+};
+
+const setupInput = function(conn) {
+  connection = conn;
+  connection.write("PTM");
+  connection.setEncoding('utf8');
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  }
-  handleUserInput();
+  stdin.on('data', handleUserInput); //callback to pass any key strokes to hUI;
   return stdin;
-};
-
-module.exports = { setupInput };
+}
+module.exports = setupInput;
